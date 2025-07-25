746d8feb935a25f75f07feb075510324f3425958
Heena9dottech <heena.9dot.tech@gmail.com>
2025-07-25 16:21:39 +0530

    Fix DocumentRoot to Laravel public directory

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/db-check', function () {
+    try {
+        DB::connection()->getPdo();
+        return "Database connected: " . DB::connection()->getDatabaseName();
+    } catch (\Exception $e) {
+        return "DB ERROR: " . $e->getMessage();
+    }
 });
