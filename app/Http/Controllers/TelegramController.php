61cb3266a4dc41024f1c18aa0b9c699e1501dace
Heena9dottech <heena.9dot.tech@gmail.com>
2025-07-26 18:11:46 +0530

    make code

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -9,7 +9,55 @@ use Illuminate\Support\Facades\Log;
 
 class TelegramController extends Controller
 {
+
     public function webhook(Request $request)
+    {
+        Log::info('Telegram Webhook Triggered');
+        Log::info($request->all());
+
+        $telegramToken = '8308817192:AAGKL7EPF-efA6z9_fQ8EUED39-FUke5cns';
+
+        $data = $request->all();
+
+        if (isset($data['message'])) {
+            $chatId = $data['message']['chat']['id'] ?? null;
+            $userMessage = strtolower(trim($data['message']['text'] ?? ''));
+
+            if ($chatId) {
+                if ($userMessage === '/start' || $userMessage === 'game') {
+                    // Send button to open Mini App
+                    Http::post("https://api.telegram.org/bot{$telegramToken}/sendMessage", [
+                        'chat_id' => $chatId,
+                        'text' => 'Click below to open the Mini App 👇',
+                        'reply_markup' => json_encode([
+                            'inline_keyboard' => [
+                                [
+                                    [
+                                        'text' => '🎮 Open Heena Mini App',
+                                        'web_app' => [
+                                            'url' => 'https://heena-tele-app.onrender.com/game'
+                                        ]
+                                    ]
+                                ]
+                            ]
+                        ])
+                    ]);
+                } else {
+                    // Just echo what user said
+                    Http::post("https://api.telegram.org/bot{$telegramToken}/sendMessage", [
+                        'chat_id' => $chatId,
+                        'text' => 'You said: ' . $userMessage,
+                    ]);
+                }
+            }
+        }
+
+        return response()->json(['status' => 'ok']);
+    }
+
+
+
+    public function webhook_old(Request $request)
     {
         Log::info('Telegram Webhook Triggered');
         Log::info($request->all());
